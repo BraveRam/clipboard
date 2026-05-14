4a4e4eaa710b802c71d854baf158914db347c4ea
Lencho Mengistu <99811385+BraveRam@users.noreply.github.com>
2026-05-14 08:49:07 +0300

    Remove comment about ignoring src-tauri in watch

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
